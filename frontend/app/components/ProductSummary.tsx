01a29e5ce05b59bc38e23a310929dc81275f224d
Teset <poom.butsricha@gmail.com>
2025-03-25 13:51:16 +0700

    ให้ใส่ low stock ไม่ได้ และ แก้ไข dashboard นิดหน่อย

--- frontend/app/components/ProductSummary.tsx
+++ frontend/app/components/ProductSummary.tsx
@@ -2,22 +2,27 @@
 import React, { useState, useEffect } from "react";
 import axios from 'axios';
 import { Package, AlertCircle, AlertTriangle, Tag } from "lucide-react";
-import { InventoryProducts, Category } from "@/types/types";
+import { InventoryProducts, Category, Product } from "@/types/types";
 import { fetchInventoryProduct, fetchCategories } from "../api/InventoryServices";
+import { fetchAllProduct } from "../api/productServices";
 
 
 export function ProductSummary() {
-  const [products, setProducts] = useState<InventoryProducts[]>([]);
+  const [stocks, setStocks] = useState<InventoryProducts[]>([]);
+  const [products, setProducts] = useState<Product[]>([]);
   const [categories, setCategories] = useState<Category[]>([]);
   const [countProducts, setCountProducts] = useState(0);
   const [countCategories, setCountCategories] = useState(0);
 
   const fetchData = async () => {
-    const productData = await fetchInventoryProduct();
-    setProducts(productData);
+    const stockData = await fetchInventoryProduct();
+    setStocks(stockData);
 
     const categoryData = await fetchCategories();
     setCategories(categoryData);
+    
+    const productData = await fetchAllProduct();
+    setProducts(productData);
   };
 
   useEffect(() => {
@@ -42,13 +47,13 @@ export function ProductSummary() {
 
       <SummaryCard
         title="Low Stock Items"
-        value={products.filter((p) => p.quantity_in_stock < 10 && p.quantity_in_stock != 0).length.toString()}
+        value={stocks.filter((p) => p.quantity_in_stock < 10 && p.quantity_in_stock != 0).length.toString()}
         icon={<AlertTriangle size={24} className="text-amber-500" />}
       />
 
       <SummaryCard
         title="Out of Stock"
-        value={products.filter((p) => p.quantity_in_stock === 0).length.toString()}
+        value={stocks.filter((p) => p.quantity_in_stock === 0).length.toString()}
         icon={<AlertCircle size={24} className="text-red-500" />}
       />
 
